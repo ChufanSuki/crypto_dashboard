c3fc1d3a9cf5b4539b8eb44687884e37690f76d4
Chufan Chen <allenplato28@gmail.com>
2021-07-14 14:27:55 +0800

    init commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import "./assets/vendor/font-awesome/css/font-awesome.css";
+import "./assets/app.scss"
 
-Vue.config.productionTip = false
+import clickOutside from "./directives/click-ouside"
+
+Vue.config.productionTip = false;
+Vue.directive("click-outside", clickOutside);
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'dashboard',
+    component: () => import('./views/Dashboard.vue')
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/view/:symbol',
+    name: 'infoview',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/InfoView.vue'),
+    // route.params will be set as the component props
+    props: true
   }
 ]
 
