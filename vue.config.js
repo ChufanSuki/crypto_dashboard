3ac690d33b65c8da777da97cd8bac5fd710740d5
Chufan Chen <50797312+RetownPlato@users.noreply.github.com>
2021-07-14 15:14:26 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 //publicPath is set as vue-crypto-dashboard for production you can change it according to your application
 module.exports = {
-  publicPath: '/crypto-dashboard/',
+  publicPath: '/crypto_dashboard/',
   filenameHashing: false
 };
