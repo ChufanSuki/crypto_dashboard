380dfd3c89506d9a66b9bfad6963bb75d261c9df
CI <CI@example.com>
2021-07-19 01:22:55 +0000

    deploy

---
+++ precache-manifest.fd60c1d5b8201a4f2fa024be37360bd2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3354dea5a5a58a750147",
+    "url": "/crypto_dashboard/css/app.css"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/crypto_dashboard/fonts/fontawesome-webfont.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/crypto_dashboard/fonts/fontawesome-webfont.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/crypto_dashboard/fonts/fontawesome-webfont.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/crypto_dashboard/fonts/fontawesome-webfont.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/crypto_dashboard/img/fontawesome-webfont.svg"
+  },
+  {
+    "revision": "d91c0dd7b984d8395fdec64bb2d474c4",
+    "url": "/crypto_dashboard/index.html"
+  },
+  {
+    "revision": "a78372ee4ead5da5d338",
+    "url": "/crypto_dashboard/js/about.js"
+  },
+  {
+    "revision": "3354dea5a5a58a750147",
+    "url": "/crypto_dashboard/js/app.js"
+  },
+  {
+    "revision": "c44b8aa54e035952cf35",
+    "url": "/crypto_dashboard/js/chunk-b76e42e8.js"
+  },
+  {
+    "revision": "5b8fd34bd610eb6f6c32",
+    "url": "/crypto_dashboard/js/chunk-vendors.js"
+  },
+  {
+    "revision": "543a6319dc5a0d42e0c1600127104395",
+    "url": "/crypto_dashboard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/crypto_dashboard/robots.txt"
+  }
+]);
